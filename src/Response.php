49c9be0939026d5bdc96c24cd69548beb7eb0f9b
yogi <a.eganov@sakh.com>
2020-09-07 18:41:07 +1100

    Доработан класс Response;

--- src/Response.php
+++ src/Response.php
@@ -2,49 +2,69 @@
 
 namespace bIbI4k0\ApplePusher;
 
+use JsonSerializable;
+
 /**
  * Class Response
  * @package ApplePusher
  */
-class Response
+class Response implements JsonSerializable
 {
     private int $code;
-    private string $reason;
-    private string $body;
+    private ?string $reason;
+    private ?array $body;
 
     /**
      * @param int $code
      * @param string $reason
-     * @param $body
+     * @param array|null $body
      */
-    public function __construct(int $code, string $reason, $body)
+    public function __construct(int $code, string $reason, array $body = null)
     {
         $this->code = $code;
         $this->reason = $reason;
         $this->body = $body;
     }
 
+    public static function fromJson(int $code, string $json): self
+    {
+        $json = json_decode($json, true, JSON_THROW_ON_ERROR);
+        return new self($code, $json['reason'] ?? null, $json);
+    }
+
     /**
      * @return bool
      */
-    public function isSuccess(): bool
+    public function isOk(): bool
     {
         return $this->code === ResponseStatus::STATUS_OK;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getReason(): string
+    public function getReason(): ?string
     {
         return $this->reason;
     }
 
     /**
-     * @return string
+     * @return array|null
      */
-    public function getBody(): string
+    public function getBody(): ?array
     {
         return $this->body;
+    }
+
+    /**
+     * @return array
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'ok' => $this->isOk(),
+            'reason' => $this->getReason(),
+            'body' => $this->getBody(),
+        ];
     }
 }
