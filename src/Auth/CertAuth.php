371865090ebee0392e671b452800060d69482585
yogi <a.eganov@sakh.com>
2020-09-07 19:50:27 +1100

    Базовая реализация авторизации через серт;

--- src/Auth/CertAuth.php
+++ src/Auth/CertAuth.php
@@ -2,8 +2,41 @@
 
 namespace bIbI4k0\ApplePusher\Auth;
 
-
-class CertAuth
+/**
+ * Class CertAuth
+ * @package bIbI4k0\ApplePusher\Auth
+ */
+class CertAuth implements AuthInterface
 {
+    private string $certFile;
+    private string $certPassword;
 
+    /**
+     * @param string $certFile
+     * @param string $certPassword
+     */
+    public function __construct(string $certFile, string $certPassword)
+    {
+        $this->certFile = $certFile;
+        $this->certPassword = $certPassword;
+    }
+
+    /**
+     * @return array
+     */
+    public function getCurlOptions(): array
+    {
+        return [
+            CURLOPT_SSLCERT => $this->certFile,
+            CURLOPT_SSLCERTPASSWD => $this->certPassword,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getRequestHeaders(): array
+    {
+        return [];
+    }
 }
