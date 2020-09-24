6437b10a3a13d5de48281feec184a2e819e9835a
yogi <a.eganov@sakh.com>
2020-09-24 16:58:16 +1100

    BaseCurlConfig учитывает ситуацию, когда расширение не имеет определения  CURLOPT_HTTP_VERSION, но libcurl поддерживает его;

--- src/BaseCurlConfig.php
+++ src/BaseCurlConfig.php
@@ -2,20 +2,50 @@
 
 namespace bIbI4k0\ApplePusher;
 
+use bIbI4k0\ApplePusher\Exception\CurlException;
+
 /**
  * Class BaseCurlConfig
  * @package bIbI4k0\ApplePusher
  */
 class BaseCurlConfig
 {
+    private const MIN_CURL_VERSION = '7.43.0';
+
     private $options = [
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2,
         CURLOPT_PORT => 443,
         CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_TIMEOUT => 3,
         CURLOPT_CONNECTTIMEOUT => 3,
     ];
+
+    /**
+     * @throws CurlException
+     */
+    public function __construct()
+    {
+        if (defined('CURL_HTTP_VERSION_2')) {
+            $curlHttpVersionConst = CURL_HTTP_VERSION_2;
+        } else {
+            // Sometimes libcurl version is fresh, but the extension was compiled for the old version of it
+            // and does not have a const CURL_HTTP_VERSION_2. In this case used value of this const = 3
+            $curlVersion = curl_version()['version'];
+            if (version_compare($curlVersion, self::MIN_CURL_VERSION) > 0) {
+                $curlHttpVersionConst = 3; // same value than CURL_HTTP_VERSION_2
+            }
+        }
+
+        if (!isset($curlHttpVersionConst)) {
+            throw new CurlException(sprintf(
+                'This version of cURL (%s) don\'t support sending via HTTP/2. Upgrade libcurl to version %s or higher.',
+                curl_version()['version'],
+                self::MIN_CURL_VERSION
+            ));
+        }
+
+        $this->set(CURLOPT_HTTP_VERSION, $curlHttpVersionConst);
+    }
 
     /**
      * @param int $optId curl option id
